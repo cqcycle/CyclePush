1d9fda2a5ff64cd6ce3b7b94121721290fd828ba
cycle <cycle@cycledeMacBook-Air.local>
2018-04-21 23:28:58 +0800

    提交

--- CycleJPush/AppDelegate+CycleJPush3_0.h
+++ CycleJPush/AppDelegate+CycleJPush3_0.h
@@ -1,12 +1,12 @@
 //
-//  AppDelegate+CycleJPush3_0.h
+//  CycleJpush3+CycleJPush3_0.h
 //  CDSBZX
 //
 //  Created by dihuijun on 17/2/28.
 //  Copyright © 2017年 Cycle. All rights reserved.
 //
 
-#import "AppDelegate.h"
+#import "CycleJpush3.h"
 
 @interface AppDelegate (CycleJPush3_0)
 
